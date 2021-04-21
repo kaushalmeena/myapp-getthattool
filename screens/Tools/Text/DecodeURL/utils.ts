c369611bc7736c25d0be20a63446bc1050e2bf2d
kaushalmeena <kaushal.meena@aasaanjobs.com>
2021-04-21 22:50:41 +0530

    fixed url encode and decode functions

--- screens/Tools/Text/DecodeURL/utils.ts
+++ screens/Tools/Text/DecodeURL/utils.ts
@@ -1,1 +1,1 @@
-export const decodeURL = (data: string): string => decodeURIComponent(data);
+export const decodeURL = (data: string): string => decodeURI(data);
--- screens/Tools/Text/EncodeURL/utils.ts
+++ screens/Tools/Text/EncodeURL/utils.ts
@@ -1,1 +1,1 @@
-export const encodeURL = (data: string): string => encodeURIComponent(data);
+export const encodeURL = (data: string): string => encodeURI(data);
