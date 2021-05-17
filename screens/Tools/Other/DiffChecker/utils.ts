3ce6417d3e31d0d0c4e295aaef076150c268d209
kaushalmeena <kaushal.meena@aasaanjobs.com>
2021-05-17 17:38:58 +0530

    updated diff-cheker utils func

--- screens/Tools/Other/DiffChecker/utils.ts
+++ screens/Tools/Other/DiffChecker/utils.ts
@@ -1,58 +1,59 @@
-export const getDiffOutput = (
-  original: string,
-  modified: string
-): string[][] => {
+export const getDiffOutput = (str1: string, str2: string): string[][] => {
   const result = [];
-  const LCSMatrix = buildLCSMatrix(original, modified);
-  let i = original.length;
-  let j = modified.length;
-  let chunk = null;
+  const LCSMatrix = buildLCSMatrix(str1, str2);
+  let firstRow = null;
+  let tempRow = null;
+  let i = str1.length;
+  let j = str2.length;
 
   while (true) {
-    if (i > 0 && j > 0 && original[i - 1] === modified[j - 1]) {
-      chunk = ["N", original[i - 1]];
+    if (i > 0 && j > 0 && str1[i - 1] === str2[j - 1]) {
+      tempRow = ["N", str1[i - 1]];
       i--;
       j--;
     } else if (
       j > 0 &&
       (i === 0 || LCSMatrix[i][j - 1] >= LCSMatrix[i - 1][j])
     ) {
-      chunk = ["G", modified[j - 1]];
+      tempRow = ["G", str2[j - 1]];
       j--;
     } else if (
       i > 0 &&
       (j === 0 || LCSMatrix[i][j - 1] < LCSMatrix[i - 1][j])
     ) {
-      chunk = ["R", original[i - 1]];
+      tempRow = ["R", str1[i - 1]];
       i--;
     } else {
       break;
     }
 
-    if (result[0] && result[0][0] && result[0][0] === chunk[0]) {
-      result[0][1] = chunk[1] + result[0][1];
+    firstRow = result[0];
+
+    if (firstRow && firstRow[0] === tempRow[0]) {
+      firstRow[1] = tempRow[1] + firstRow[1];
+      result[0] = firstRow;
       continue;
     }
 
-    result.unshift(chunk);
+    result.unshift(tempRow);
   }
 
   return result;
 };
 
-function buildLCSMatrix(original: string, modified: string) {
+function buildLCSMatrix(str1: string, str2: string) {
   const LCSMatrix = [];
 
-  for (let x = 0; x <= original.length; x++) {
+  for (let x = 0; x <= str1.length; x++) {
     LCSMatrix[x] = [];
-    for (let y = 0; y <= modified.length; y++) {
+    for (let y = 0; y <= str2.length; y++) {
       LCSMatrix[x][y] = 0;
     }
   }
 
-  for (let i = 1; i <= original.length; i++) {
-    for (let j = 1; j <= modified.length; j++) {
-      if (original[i - 1] === modified[j - 1]) {
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
         LCSMatrix[i][j] = LCSMatrix[i - 1][j - 1] + 1;
       } else {
         LCSMatrix[i][j] = Math.max(LCSMatrix[i - 1][j], LCSMatrix[i][j - 1]);
