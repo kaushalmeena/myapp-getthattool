e6a6c76139f4f96bb196ad72fd01ec92ecc92fb4
kaushalmeena <kaushal.meena1996@gmail.com>
2022-06-14 23:58:11 +0530

    made toast fixed

--- src/shared/Toast/index.tsx
+++ src/shared/Toast/index.tsx
@@ -3,6 +3,9 @@ import { Position, Toaster } from "@blueprintjs/core";
 const Toast =
   typeof window === "undefined"
     ? null
-    : Toaster.create({ position: Position.BOTTOM_RIGHT });
+    : Toaster.create({
+        position: Position.BOTTOM_RIGHT,
+        className: "position-fixed"
+      });
 
 export default Toast;
